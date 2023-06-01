0b0bc00d1ca11d29c254a67c8fdc2d60ca1c0456
Andrei Puchko <andrei.puchko@gmx.de>
2023-06-01 08:50:20 -0700

    mac dmg

--- build_executable.zsh
+++ build_executable.zsh
@@ -1,1 +1,14 @@
 pyinstaller --onefile --noconsole --icon=q2rad.ico --distpath ./bin/macos ./q2radlauncher/q2radlauncher.py
+rm bin/macos/q2radlauncher
+create-dmg \
+  --volname "q2radlauncher Installer" \
+  --volicon "q2rad.ico" \
+  --window-pos 200 120 \
+  --window-size 800 400 \
+  --icon-size 100 \
+  --icon "q2rad.ico" 200 190 \
+  --hide-extension "q2radlauncher.app" \
+  --app-drop-link 600 185 \
+  "bin/macos/q2radlauncher-Installer.dmg" \
+  "bin/macos"
+  rm -r bin/macos/q2radlauncher.app
